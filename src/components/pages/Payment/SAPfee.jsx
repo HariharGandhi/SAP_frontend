551a5ffca7614dcdb053f16b02cf2fbd4688d78a
HariharGandhi <94119593+HariharGandhi@users.noreply.github.com>
2023-04-18 08:32:00 +0530

    sapfee navbar logindone minimal changes

--- src/components/pages/Payment/SAPfee.jsx
+++ src/components/pages/Payment/SAPfee.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./SAPfee.css";
+import NewSidebar from "../../Navbar/Navbar";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
 const SAPfee = () => {
     useEffect(() => {
         return () => sessionStorage.setItem('sidebar',JSON.stringify(false));
       });
     return (
         <>
-            <div className="container">
-                <h1>Click on Make Payment</h1>
-                <h4>You will be redirected to Payment Form</h4>
+            <NewSidebar />
+            <Card className="sapcontainer" id="sapfee">
+            <CardHeader title={<h1>SAP Payment</h1>}>
+                </CardHeader>
+            <CardContent>
+                <h4>Click on Make payment to proceed for payment</h4>
+                <h6>You will be redirected to Payment Form</h6>
+                <h1>{""}</h1>
                 <button>
                 <a href='https://docs.google.com/forms/d/1aUcjAvH_puK9lz5fUzrvYZHc_tV23n5wtz0pSpNrJOI/viewform?ts=62975728&edit_requested=true ' target="_blank"rel="noopener noreferrer">Make Payment</a>
             </button>
-            <button> <Link to="/logindone">Back to Home</Link></button>
-            </div>
+            <button style={{marginLeft:'350px'}}> <Link to="/logindone">Cancel</Link></button></CardContent>
+            </Card>
         </>
     )
 }
--- src/components/pages/dashboard/Studentdashboard/logindone.component.jsx
+++ src/components/pages/dashboard/Studentdashboard/logindone.component.jsx
@@ -1,133 +1,135 @@
-import React, { } from "react";
+import React from "react";
 import NewSidebar from "../../../Navbar/Navbar";
 import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 
-
-
-
-
 const Logindone = () => {
   const history = useHistory();
 
-  const NevigatePayment = () =>{ 
-    history.push('/payment')
-  }
-  const NevigateResume = () =>{ 
-    history.push('/resume')
-  }
+  const NevigatePayment = () => {
+    history.push("/payment");
+  };
+  const NevigateResume = () => {
+    history.push("/resume");
+  };
 
-  const NevigateApplicationform = () =>{ 
-    history.push('/updateprofile')
-  }
+  const NevigateApplicationform = () => {
+    history.push("/updateprofile");
+  };
 
-  const NevigateCourseDetails = () =>{ 
-    history.push('/coursedetails')
-  }
-  const NevigateTwoWayCommunication = () =>{ 
-    history.push('/contacttoadmin')
-  }
-  const NevigateAdminDetails = () =>{ 
-    history.push('/admindetails')
-  }
+  const NevigateCourseDetails = () => {
+    history.push("/coursedetails");
+  };
+  const NevigateTwoWayCommunication = () => {
+    history.push("/contacttoadmin");
+  };
+  const NevigateAdminDetails = () => {
+    history.push("/admindetails");
+  };
   useEffect(() => {
-    return () => sessionStorage.setItem('sidebar',JSON.stringify(false));
+    return () => sessionStorage.setItem("sidebar", JSON.stringify(false));
   });
-
-  
-
 
   return (
     <>
       <NewSidebar />
-      <section className={
+      <section
+        className={
           sessionStorage.getItem("sidebar") === "true"
             ? "services vform"
             : "services"
-        } id="services">
+        }
+        id="services"
+      >
         <div className="content">
-          <div className="title">
-
-          </div>
-          <div className="boxes" >
+          <div className="title"></div>
+          <div className="boxes">
             <div
               className="box"
               data-aos="fade-down-right"
               data-aos-duration="2000"
               data-aos-delay="1000"
+              onClick={() => NevigatePayment()}
+              style={{ cursor: "pointer" }}
             >
               <div className="icon">
                 <i className="fas fa-desktop"></i>
               </div>
-              <div onClick={() => NevigatePayment()} className="topic">Payment</div>
-              <p>
-                This is your payment Section
-              </p>
+              <div className="topic">Payment</div>
+              <p>This is your payment Section</p>
             </div>
-            <div className="box" data-aos="fade-down-right" data-aos-delay="300">
+            <div
+              className="box"
+              data-aos="fade-down-right"
+              data-aos-delay="300"
+              style={{ cursor: "pointer" }}
+              onClick={() => NevigateResume()}
+            >
               <div className="icon">
                 <i className="fas fa-brush"></i>
               </div>
-              <div onClick={() => NevigateResume()} className="topic">Resume</div>
-              <p>
-                Create your resume
-              </p>
+              <div className="topic">Resume</div>
+              <p>Create your resume</p>
             </div>
-            <div className="box" data-aos="fade-down-right" data-aos-delay="500">
+            <div
+              className="box"
+              data-aos="fade-down-right"
+              data-aos-delay="500"
+              style={{ cursor: "pointer" }}
+              onClick={() => NevigateApplicationform()}
+            >
               <div className="icon">
                 <i className="fas fa-chart-line"></i>
               </div>
-              <div onClick={() => NevigateApplicationform()} className="topic">Application Form</div>
-              <p>
-                this is your application form card
-              </p>
+              <div className="topic">Application Form</div>
+              <p>this is your application form card</p>
             </div>
             <div
               className="box"
               data-aos="fade-down-right"
               data-aos-delay="100"
               data-aos-offset="120"
+              style={{ cursor: "pointer" }}
+              onClick={() => NevigateCourseDetails()}
             >
               <div className="icon">
                 <i className="fab fa-wordpress"></i>
               </div>
-              <div onClick={() => NevigateCourseDetails()} className="topic">Course Details</div>
-              <p>
-               Course Details card
-              </p>
+              <div className="topic">Course Details</div>
+              <p>Course Details card</p>
             </div>
             <div
               className="box"
               data-aos="fade-down-right"
               data-aos-delay="300"
               data-aos-offset="120"
+              style={{ cursor: "pointer" }}
+              onClick={() => NevigateTwoWayCommunication()}
             >
               <div className="icon">
                 <i className="fas fa-cloud"></i>
               </div>
-              <div onClick={() => NevigateTwoWayCommunication()} className="topic">Query Section</div>
-              <p>
-                2 way communication card
-              </p>
+              <div className="topic">Query Section</div>
+              <p>2 way communication card</p>
             </div>
             <div
               className="box"
               data-aos="fade-down-right"
               data-aos-delay="500"
               data-aos-offset="120"
+              style={{ cursor: "pointer" }}
+              onClick={() => NevigateAdminDetails()}
             >
               <div className="icon">
                 <i className="fas fa-tablet-alt"></i>
               </div>
-              <div onClick={() => NevigateAdminDetails()} className="topic">Admin Details </div>
-              <p>
-                Admin datails For Contact
-              </p>
+              <div className="topic">Admin Details </div>
+              <p>Admin datails For Contact</p>
             </div>
           </div>
         </div>
       </section>
     </>
   );
-}
+};
 export default Logindone;
