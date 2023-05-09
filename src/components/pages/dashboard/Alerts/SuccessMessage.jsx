74e809e7f32c826c8834cac1450e5f4d44bd52e1
Sachin Jagtap <100439480+Sachin3302@users.noreply.github.com>
2023-05-09 14:04:12 +0530

    css and error message changes

--- src/components/pages/dashboard/Alerts/SuccessMessage.jsx
+++ src/components/pages/dashboard/Alerts/SuccessMessage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./SuccessMessage.css";
 const SuccessMessage = ({ message }) => {
     return (
       <div
--- src/components/pages/dashboard/Studentdashboard/SidebarForStudent/Home/Six_Card_Pages/contactToAdmin.jsx
+++ src/components/pages/dashboard/Studentdashboard/SidebarForStudent/Home/Six_Card_Pages/contactToAdmin.jsx
@@ -6,6 +6,7 @@ import Axios from 'axios';
 import { BASE_URL } from '../../../../../../../services/Globalvalues';
 import { useState } from 'react';
 
+
 const ContactToAdmin = () => {
   const [SapUsername, setSapUsername] = useState("")
   const [SapPassword, setSapPassword] = useState("")
@@ -26,22 +27,44 @@ const ContactToAdmin = () => {
     }})();
     return () => sessionStorage.setItem('sidebar',JSON.stringify(false));
 },[UId]);
+
+const handleUploadClick = () => {
+  window.location.href ="/getinstallment";
+}
+
+
   return (
-    <>
-            <NewSidebar />
-            <Card className="sapcontainer" id="sapfee" style={{ backgroundColor: '#f2f2f2', borderRadius: '10px', padding: '20px' }}>
-            <CardHeader title={<h1 style={{ color: '#333333', fontWeight: 'bold' }}>Login credentials of SAP</h1>}>
-                </CardHeader>
-            <CardContent>
-                <h2 style={{ color: '#666666' }}>UserName: <span style={{ fontStyle: 'italic', color: '#333333' }}>{SapUsername}</span></h2>
-                <h2 style={{ color: '#666666' }}>Password: <span style={{ fontStyle: 'italic', color: '#333333' }}>{SapPassword}</span></h2>
-                <h1>{""}</h1>
-                {/* <button>
-                
-            </button>
-            <button style={{marginLeft:'350px'}}> <Link to="/logindone">Cancel</Link></button>*/} </CardContent> 
-            </Card>
-        </>
+
+<>
+  <NewSidebar />
+  <Card className="sapcontainer" id="sapfee" style={{ backgroundColor: '#f2f2f2', borderRadius: '10px', padding: '20px' }}>
+  <h2  style={{ color: '#666666', marginBottom: '10px' }}>
+    {SapUsername ? (
+      <>
+        This is your credentials
+      </>
+    ) : (
+      <>
+        Pay your fees else upload your payment receipt
+        <button onClick={handleUploadClick}  style={{ marginLeft: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', padding: '5px 10px' }}>Upload Now</button>
+      </>
+    )}
+  </h2>
+    <CardHeader title={<h1 style={{ color: '#333333', fontWeight: 'bold' }}>Login credentials of SAP</h1>}>
+    </CardHeader>
+    <CardContent>
+      <h2 style={{ color: '#666666' }}>UserName: <span style={{ fontStyle: 'italic', color: '#333333' }}>{SapUsername}</span></h2>
+      <h2 style={{ color: '#666666' }}>Password: <span style={{ fontStyle: 'italic', color: '#333333' }}>{SapPassword}</span></h2>
+      <h1>{""}</h1>
+      {/* <button> </button> */}
+      {/* <button style={{marginLeft:'350px'}}> <Link to="/logindone">Cancel</Link></button>*/}
+    </CardContent> 
+  </Card>
+</>
+
+
+
+
   );
 }
 
